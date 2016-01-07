a57466d29deef4ae7277ff1091aba434df901bc9
cooperdow <dowco@mullenmail.com>
2016-01-07 08:37:01 -0700

    asdas

--- pages/weatherApp.js
+++ pages/weatherApp.js
@@ -1,2 +1,16 @@
 //called the module method on the angular object
 var myApp = angular.module('weatherApp', []);
+//calling a controller method on myApp, array takes name of service that is being passed in to the function
+myApp.controller("homeController", ["$scope", "cityService", function($scope, cityService){
+    $scope.city = cityService.city;
+    //watch method takes in two paramaters, like most methods: name, function
+    $scope.watch('city', function(){
+        cityService.city = $scope.city;
+    })
+    
+}])
+
+//creating a new service by calling the service method
+myApp.service('cityService', function(){
+    this.city = "Denver, Co";
+});
